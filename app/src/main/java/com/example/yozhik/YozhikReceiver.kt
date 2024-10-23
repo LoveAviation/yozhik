87f2d4ec7d80d2a49e4ff2c473fa6c85a5690847
LoveAviation <ivangeorgmarcov@gmail.com>
2024-10-23 19:36:36 +0300

    ready?

--- app/src/main/java/com/example/yozhik/YozhikReceiver.kt
+++ app/src/main/java/com/example/yozhik/YozhikReceiver.kt
@@ -10,21 +10,31 @@ import android.widget.Toast
 class YozhikReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        val message = intent?.getStringExtra("message")
-        if (message == "Yozhik!") {
+        val handler = Handler(Looper.getMainLooper())
+        val result = goAsync()
+        val thread: Thread = object : Thread() {
+            override fun run() {
+                val message = intent?.getStringExtra("message")
+                if (message == "Yozhik!") {
 
-            if (context is MainActivity) {
-                context.incrementMessageCount()
+                    if (context is MainActivity) {
+                        handler.post{
+                            context.incrementMessageCount()
+                        }
+                    }
+
+                    sleep(5000)
+
+                    val intentBack = Intent("com.example.SECRET_MESSAGE").apply {
+                        putExtra("message", "Loshadka!")
+                        setPackage("com.example.loshadka")
+                    }
+                    context?.sendBroadcast(intentBack)
+                }
+                result.finish()
             }
-
-            Thread.sleep(5000)
-
-            val intentBack = Intent("com.example.SECRET_MESSAGE").apply {
-                putExtra("message", "Loshadka!")
-                setPackage("com.example.loshadka")
-            }
-            context?.sendBroadcast(intentBack)
         }
+        thread.start()
     }
 
 }
